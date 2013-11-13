192a9033cf5e3182e1d5f26214d5ff9764c814f2
Megan Durham <megan@mobiusconsortium.org>
2013-11-13 13:44:14 -0600

    add 856links.js and remove 856 from briefcit

---
+++ 856links.js
@@ -0,0 +1,32 @@
+function hideLinks(){
+    
+    var stringsToHide = new Array ();
+    stringsToHide[1] = new Array ("mineralarea");
+    stringsToHide[2] = new Array ("trcc");
+    stringsToHide[3] = new Array ("semo");
+
+
+    var scopeDropdown = $("#searchscope").val();
+    
+    delete stringsToHide[scopeDropdown];
+	
+    if ($("table").is('.bibLinks')){
+	for (var k in stringsToHide) {
+	    for (var l in stringsToHide[k]) {
+		$(".bibLinks a").each(function(index, element) {
+		    if($(element).parent().html().indexOf(stringsToHide[k][l]) > -1) {
+			$(element).parent().parent().hide()
+		    }    
+		});
+	    }
+	}
+	if ($(".bibLinks tr[style*=none]").length < $(".bibLinks a").length){
+	    $(".bibLinks").show();       
+	}
+    }  
+}
+    
+    
+    $(document).ready(function () {
+	hideLinks();
+});
